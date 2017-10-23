113106b601d2fbc15d3a0a4169b7ae18d87a2219
Stoian Dan <danmihaistoian@outlook.com>
2017-10-23 21:00:57 +0300

    AirportBean

---
+++ FlightManager/controller/src/main/java/ro/uvt/controller/beans/AirportBean.java
@@ -0,0 +1,49 @@
+package ro.uvt.controller.beans;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import ro.uvt.entity.Airport;
+import ro.uvt.entity.dao.Active;
+
+/**
+ *
+ * @author dan
+ */
+@Stateless
+public class AirportBean {
+    
+    @Inject
+    private Active dao;
+    
+    
+    private final Class<Airport> TYPE = Airport.class;
+    
+    public void create(Airport airport){
+        dao.create(airport);
+    }
+    
+    public void update(Airport airport){
+        dao.update(airport);
+    }
+    
+    public void remove(Airport airport){
+        dao.remove(airport);
+    }
+    
+    public void removeById(long id){
+        dao.removeById(TYPE, id);
+    }
+    
+    public List<Airport> findAll(){
+        return dao.findAll(TYPE);
+    }
+    
+    public Airport findById(long id){
+        return dao.findById(TYPE, id);
+    }
+    
+    public long count(){
+       return dao.count(TYPE);
+    }
+}
