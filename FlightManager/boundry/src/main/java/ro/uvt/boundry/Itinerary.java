c08716722f2793e6098ff3355651138b38da8963
Anastasia Sabau <anastasia.sabau@gmail.com>
2018-02-27 15:11:07 +0200

    itinerary page

---
+++ FlightManager/boundry/src/main/java/ro/uvt/boundry/Itinerary.java
@@ -0,0 +1,51 @@
+package ro.uvt.boundry;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import lombok.Data;
+import org.primefaces.event.RowEditEvent;
+import ro.uvt.controller.beans.ItineraryBean;
+
+/**
+ *
+ * @author Anastasia
+ */
+
+@Named("itinerary")
+@Data
+@SessionScoped
+public class Itinerary implements Serializable{
+    
+     @Inject
+    private ItineraryBean itineraryBean;
+    
+    private ro.uvt.entity.Itinerary entity = new ro.uvt.entity.Itinerary();
+    
+    List<ro.uvt.entity.Itinerary> itineraryList;
+    
+    public void submit() {
+        itineraryBean.create(entity);
+        entity = new ro.uvt.entity.Itinerary();
+    }
+    
+     public void clear() {
+        entity.setName("");
+     }
+     
+     @PostConstruct
+    public void init() {
+       itineraryList = itineraryBean.findAll();
+    }
+    
+     public void onRowEdit(RowEditEvent event) {
+        itineraryBean.update((ro.uvt.entity.Itinerary) event.getObject());
+    }
+
+    public void onRowDelete(ro.uvt.entity.Itinerary itinerary) {
+        itineraryBean.removeById(itinerary.getId());
+    }
+}
