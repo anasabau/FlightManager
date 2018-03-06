7fb2fb4a765c45648014006fd536d2d05aeb23ab
Anastasia Sabau <anastasia.sabau@gmail.com>
2018-03-06 20:51:54 +0200

    Update - User page

--- FlightManager/boundry/src/main/java/ro/uvt/boundry/User.java
+++ FlightManager/boundry/src/main/java/ro/uvt/boundry/User.java
@@ -5,11 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.Data;
-import org.primefaces.event.RowEditEvent;
+import org.primefaces.event.CellEditEvent;
 import ro.uvt.controller.beans.CompanyBean;
 import ro.uvt.controller.beans.RoleBean;
 import ro.uvt.controller.beans.UserBean;
@@ -18,10 +18,9 @@ import ro.uvt.controller.beans.UserBean;
  *
  * @author Anastasia
  */
-
 @Named("user")
 @Data
-@SessionScoped
+@RequestScoped
 public class User implements Serializable {
 
     @Inject
@@ -45,11 +44,18 @@ public class User implements Serializable {
 
     private String roleSelectedId;
 
-    public void submit() {
-        entity.setCompany_id(companyBean.findById(Long.decode(companySelectedId)));
-        entity.setRole_id(roleBean.findById(Long.decode(roleSelectedId)));
-        userBean.create(entity);
-        entity = new ro.uvt.entity.Users();
+    private ro.uvt.entity.Users selectedUser;
+
+    @PostConstruct
+    public void init() {
+        userList = userBean.findAll();
+        for (ro.uvt.entity.Company comp : companyBean.findAll()) {
+            companies.put(comp.getName() + " (id " + comp.getId() + " )", comp.getId());
+        }
+
+        for (ro.uvt.entity.Roles role : roleBean.findAll()) {
+            roles.put(role.getName() + " (id " + role.getId() + " )", role.getId());
+        }
     }
 
     public void clear() {
@@ -62,24 +68,22 @@ public class User implements Serializable {
 
     }
 
-    @PostConstruct
-    public void init() {
-        userList = userBean.findAll();
-        for (ro.uvt.entity.Company comp : companyBean.findAll()) {
-            companies.put(comp.getName() + " (id " + comp.getId() + " )", comp.getId());
-        }
-        
-        for (ro.uvt.entity.Roles role : roleBean.findAll()) {
-            roles.put(role.getName() + " (id " + role.getId() + " )", role.getId());
-        }
+    public void submit() {
+        entity.setCompany_id(companyBean.findById(Long.decode(companySelectedId)));
+        entity.setRole_id(roleBean.findById(Long.decode(roleSelectedId)));
+        userBean.create(entity);
+        entity = new ro.uvt.entity.Users();
     }
 
-    public void onRowEdit(RowEditEvent event) {
-        userBean.update((ro.uvt.entity.Users) event.getObject());
+    public void onCellEdit(CellEditEvent event) {
+        userBean.update((ro.uvt.entity.Users) userList.get(event.getRowIndex()));
     }
 
-    public void onRowDelete(ro.uvt.entity.Users user) {
-        userBean.setActive(user, false);
+    public void onRowDelete() {
+        if (selectedUser != null) {
+            userList.remove(selectedUser);
+            userBean.setActive(selectedUser, false);
+        }
     }
 
 }
