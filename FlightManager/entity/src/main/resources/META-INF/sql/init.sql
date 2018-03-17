dceb3a94a924ba3797ea4a1c7ae12b51a006757d
Stoian <danmihaistoian@outlook.com>
2018-03-17 17:47:21 +0200

    begining mocking data

---
+++ FlightManager/entity/src/main/resources/META-INF/sql/init.sql
@@ -0,0 +1,121 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  dan
+ * Created: Mar 17, 2018
+ */
+
+/*
+*           --------------------- COMPANY ------------------
+*/
+
+create table company (
+	id INT,
+	name VARCHAR(50),
+	country VARCHAR(50),
+	headquarters VARCHAR(50),
+	is_active INT
+);
+insert into company (id, name, country, headquarters, is_active) values (1, 'Avavee', 'China', 'Sunzhuang', 1);
+insert into company (id, name, country, headquarters, is_active) values (2, 'Gabspot', 'Bangladesh', 'Saidpur', 1);
+insert into company (id, name, country, headquarters, is_active) values (3, 'Jaxspan', 'Yemen', 'Aş Şalw', 1);
+insert into company (id, name, country, headquarters, is_active) values (4, 'Edgeify', 'Bangladesh', 'Tāngāil', 1);
+insert into company (id, name, country, headquarters, is_active) values (5, 'Katz', 'Mexico', 'San Agustin', 1);
+insert into company (id, name, country, headquarters, is_active) values (6, 'Meedoo', 'Kazakhstan', 'Aksu', 1);
+insert into company (id, name, country, headquarters, is_active) values (7, 'Thoughtworks', 'France', 'Tours', 1);
+insert into company (id, name, country, headquarters, is_active) values (8, 'Skipfire', 'Indonesia', 'Tiyingtali Kelod', 1);
+insert into company (id, name, country, headquarters, is_active) values (9, 'Twitternation', 'Nigeria', 'Udi', 1);
+insert into company (id, name, country, headquarters, is_active) values (10, 'Eabox', 'Portugal', 'Picoto', 1);
+insert into company (id, name, country, headquarters, is_active) values (11, 'Mynte', 'China', 'Qilong', 1);
+insert into company (id, name, country, headquarters, is_active) values (12, 'Livetube', 'Brazil', 'Canindé', 1);
+insert into company (id, name, country, headquarters, is_active) values (13, 'Yamia', 'Argentina', 'Santo Tomé', 1);
+insert into company (id, name, country, headquarters, is_active) values (14, 'Demimbu', 'China', 'Daduo', 1);
+insert into company (id, name, country, headquarters, is_active) values (15, 'Mita', 'Brazil', 'Paraguaçu Paulista', 1);
+insert into company (id, name, country, headquarters, is_active) values (16, 'Fivebridge', 'Bosnia and Herzegovina', 'Donja Dubica', 1);
+insert into company (id, name, country, headquarters, is_active) values (17, 'Minyx', 'China', 'Bijie', 1);
+insert into company (id, name, country, headquarters, is_active) values (18, 'Roombo', 'Bulgaria', 'Pleven', 1);
+insert into company (id, name, country, headquarters, is_active) values (19, 'Yombu', 'Russia', 'Povenets', 1);
+insert into company (id, name, country, headquarters, is_active) values (20, 'Yadel', 'China', 'Lishan', 1);
+insert into company (id, name, country, headquarters, is_active) values (21, 'Zoombeat', 'China', 'Liuji', 1);
+insert into company (id, name, country, headquarters, is_active) values (22, 'Skynoodle', 'Indonesia', 'Danauparis', 1);
+insert into company (id, name, country, headquarters, is_active) values (23, 'Latz', 'France', 'Cluses', 1);
+insert into company (id, name, country, headquarters, is_active) values (24, 'Oyonder', 'Colombia', 'Jamundí', 1);
+insert into company (id, name, country, headquarters, is_active) values (25, 'Kanoodle', 'Russia', 'Kostrovo', 1);
+insert into company (id, name, country, headquarters, is_active) values (26, 'Gabcube', 'Portugal', 'Zambujal', 1);
+insert into company (id, name, country, headquarters, is_active) values (27, 'Photofeed', 'Portugal', 'Rana', 1);
+insert into company (id, name, country, headquarters, is_active) values (28, 'Devshare', 'Tajikistan', 'Boshkengash', 1);
+insert into company (id, name, country, headquarters, is_active) values (29, 'Kaymbo', 'Brazil', 'Vitória de Santo Antão', 1);
+insert into company (id, name, country, headquarters, is_active) values (30, 'Snaptags', 'Sweden', 'Stockholm', 1);
+insert into company (id, name, country, headquarters, is_active) values (31, 'Aivee', 'Portugal', 'Monte da Pedra', 1);
+insert into company (id, name, country, headquarters, is_active) values (32, 'Buzzdog', 'Russia', 'Krasnogvardeyets', 1);
+insert into company (id, name, country, headquarters, is_active) values (33, 'Blogtags', 'Portugal', 'Santa Marta de Penaguião', 1);
+insert into company (id, name, country, headquarters, is_active) values (34, 'Aibox', 'Sweden', 'Stockholm', 1);
+insert into company (id, name, country, headquarters, is_active) values (35, 'Yodo', 'Indonesia', 'Teongtoda', 1);
+insert into company (id, name, country, headquarters, is_active) values (36, 'Tagcat', 'Russia', 'Pushkino', 1);
+insert into company (id, name, country, headquarters, is_active) values (37, 'Vinte', 'Indonesia', 'Lewokluok', 1);
+insert into company (id, name, country, headquarters, is_active) values (38, 'Shufflester', 'Indonesia', 'Bukbatang', 1);
+insert into company (id, name, country, headquarters, is_active) values (39, 'Tagcat', 'France', 'Vitrolles', 1);
+insert into company (id, name, country, headquarters, is_active) values (40, 'Eimbee', 'Portugal', 'Souto', 1);
+insert into company (id, name, country, headquarters, is_active) values (41, 'Babbleopia', 'South Korea', 'Kurye', 1);
+insert into company (id, name, country, headquarters, is_active) values (42, 'Demizz', 'Japan', 'Ebetsu', 1);
+insert into company (id, name, country, headquarters, is_active) values (43, 'Skipfire', 'Sweden', 'Ljungby', 1);
+insert into company (id, name, country, headquarters, is_active) values (44, 'Dynazzy', 'Vietnam', 'Tây Ninh', 1);
+insert into company (id, name, country, headquarters, is_active) values (45, 'Skyble', 'Indonesia', 'Cikembar', 1);
+insert into company (id, name, country, headquarters, is_active) values (46, 'Oba', 'China', 'Laba Goumen', 1);
+insert into company (id, name, country, headquarters, is_active) values (47, 'Shufflester', 'China', 'Yonggu', 1);
+insert into company (id, name, country, headquarters, is_active) values (48, 'Quatz', 'Philippines', 'Batad', 1);
+insert into company (id, name, country, headquarters, is_active) values (49, 'Oodoo', 'Pakistan', 'Shahkot', 1);
+insert into company (id, name, country, headquarters, is_active) values (50, 'Talane', 'Ukraine', 'Medenychi', 1);
+insert into company (id, name, country, headquarters, is_active) values (51, 'Avamm', 'Indonesia', 'Cimuncang', 1);
+insert into company (id, name, country, headquarters, is_active) values (52, 'Skippad', 'Lithuania', 'Aleksotas', 1);
+insert into company (id, name, country, headquarters, is_active) values (53, 'Digitube', 'Philippines', 'Alegria', 1);
+insert into company (id, name, country, headquarters, is_active) values (54, 'Eabox', 'Argentina', 'San Pedro', 1);
+insert into company (id, name, country, headquarters, is_active) values (55, 'Meemm', 'Cuba', 'Guantánamo', 1);
+insert into company (id, name, country, headquarters, is_active) values (56, 'Twitterwire', 'Jamaica', 'Bethel Town', 1);
+insert into company (id, name, country, headquarters, is_active) values (57, 'Avamba', 'Guatemala', 'Santa Cruz Muluá', 1);
+insert into company (id, name, country, headquarters, is_active) values (58, 'Avaveo', 'Czech Republic', 'Dolní Dunajovice', 1);
+insert into company (id, name, country, headquarters, is_active) values (59, 'Abatz', 'Russia', 'Baykalovo', 1);
+insert into company (id, name, country, headquarters, is_active) values (60, 'Realblab', 'China', 'Lüyang', 1);
+insert into company (id, name, country, headquarters, is_active) values (61, 'Cogilith', 'Philippines', 'Usab', 1);
+insert into company (id, name, country, headquarters, is_active) values (62, 'Yabox', 'Poland', 'Żarki', 1);
+insert into company (id, name, country, headquarters, is_active) values (63, 'Gabcube', 'Iran', 'Fāryāb', 1);
+insert into company (id, name, country, headquarters, is_active) values (64, 'Avaveo', 'Argentina', 'Berón de Astrada', 1);
+insert into company (id, name, country, headquarters, is_active) values (65, 'Linklinks', 'Germany', 'Bochum', 1);
+insert into company (id, name, country, headquarters, is_active) values (66, 'Ntag', 'Russia', 'Magnitogorsk', 1);
+insert into company (id, name, country, headquarters, is_active) values (67, 'Riffpedia', 'China', 'Sishan', 1);
+insert into company (id, name, country, headquarters, is_active) values (68, 'Photolist', 'Russia', 'Zyuzino', 1);
+insert into company (id, name, country, headquarters, is_active) values (69, 'Blogpad', 'Afghanistan', 'Asmar', 1);
+insert into company (id, name, country, headquarters, is_active) values (70, 'Realmix', 'Pakistan', 'Moro', 1);
+insert into company (id, name, country, headquarters, is_active) values (71, 'Skalith', 'France', 'Cergy-Pontoise', 1);
+insert into company (id, name, country, headquarters, is_active) values (72, 'Dynabox', 'China', 'Qinglung', 1);
+insert into company (id, name, country, headquarters, is_active) values (73, 'Ooba', 'Indonesia', 'Kadupayung', 1);
+insert into company (id, name, country, headquarters, is_active) values (74, 'Skidoo', 'Sweden', 'Karlskoga', 1);
+insert into company (id, name, country, headquarters, is_active) values (75, 'Babbleblab', 'Philippines', 'Kalawit', 1);
+insert into company (id, name, country, headquarters, is_active) values (76, 'Fiveclub', 'Indonesia', 'Pekalongan', 1);
+insert into company (id, name, country, headquarters, is_active) values (77, 'Thoughtsphere', 'France', 'Sélestat', 1);
+insert into company (id, name, country, headquarters, is_active) values (78, 'Thoughtstorm', 'Serbia', 'Sremska Mitrovica', 1);
+insert into company (id, name, country, headquarters, is_active) values (79, 'Miboo', 'Panama', 'Cerro Punta', 1);
+insert into company (id, name, country, headquarters, is_active) values (80, 'Dabjam', 'Indonesia', 'Cibiru', 1);
+insert into company (id, name, country, headquarters, is_active) values (81, 'Flipopia', 'Argentina', 'Concepción', 1);
+insert into company (id, name, country, headquarters, is_active) values (82, 'Camido', 'Indonesia', 'Waihibar', 1);
+insert into company (id, name, country, headquarters, is_active) values (83, 'Mydo', 'Nigeria', 'Zango', 1);
+insert into company (id, name, country, headquarters, is_active) values (84, 'Katz', 'China', 'Daping', 1);
+insert into company (id, name, country, headquarters, is_active) values (85, 'Omba', 'Bulgaria', 'Bukhovo', 1);
+insert into company (id, name, country, headquarters, is_active) values (86, 'Shuffletag', 'Indonesia', 'Batangtoru', 1);
+insert into company (id, name, country, headquarters, is_active) values (87, 'Divanoodle', 'Portugal', 'Requião', 1);
+insert into company (id, name, country, headquarters, is_active) values (88, 'Skajo', 'Ukraine', 'Berestechko', 1);
+insert into company (id, name, country, headquarters, is_active) values (89, 'Camimbo', 'Finland', 'Pudasjärvi', 1);
+insert into company (id, name, country, headquarters, is_active) values (90, 'Devcast', 'Philippines', 'Upper San Mateo', 1);
+insert into company (id, name, country, headquarters, is_active) values (91, 'Yadel', 'Honduras', 'Reitoca', 1);
+insert into company (id, name, country, headquarters, is_active) values (92, 'Edgeclub', 'Canada', 'Leduc', 1);
+insert into company (id, name, country, headquarters, is_active) values (93, 'Buzzdog', 'France', 'Saint-Denis', 1);
+insert into company (id, name, country, headquarters, is_active) values (94, 'Tagcat', 'Indonesia', 'Sikka', 1);
+insert into company (id, name, country, headquarters, is_active) values (95, 'Latz', 'Indonesia', 'Titiakar', 1);
+insert into company (id, name, country, headquarters, is_active) values (96, 'Meevee', 'Indonesia', 'Kasonawejo', 1);
+insert into company (id, name, country, headquarters, is_active) values (97, 'Fatz', 'Russia', 'Nikulino', 1);
+insert into company (id, name, country, headquarters, is_active) values (98, 'Thoughtstorm', 'Philippines', 'Bungsuan', 1);
+insert into company (id, name, country, headquarters, is_active) values (99, 'Eazzy', 'China', 'Geyuan', 1);
+insert into company (id, name, country, headquarters, is_active) values (100, 'Voonyx', 'China', 'Rifeng', 1);
