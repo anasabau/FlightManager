566b123ae0ff0c659a09297e1ac90568c9363ffc
Stoian <danmihaistoian@outlook.com>
2018-03-21 14:37:00 +0200

    unneded import

--- FlightManager/boundary/src/main/java/ro/uvt/boundry/Itinerary.java
+++ FlightManager/boundary/src/main/java/ro/uvt/boundry/Itinerary.java
@@ -3,7 +3,6 @@ package ro.uvt.boundry;
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
