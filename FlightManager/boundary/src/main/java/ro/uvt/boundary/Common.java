2227aaf59e5f8aea4bf88db0711bcbd2a1b8ad04
Stoian <danmihaistoian@outlook.com>
2018-06-20 20:13:47 +0300

    added response in case of failed login

--- FlightManager/boundary/src/main/java/ro/uvt/boundary/Common.java
+++ FlightManager/boundary/src/main/java/ro/uvt/boundary/Common.java
@@ -5,6 +5,7 @@
  */
 package ro.uvt.boundary;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.security.NoSuchAlgorithmException;
 import javax.enterprise.context.RequestScoped;
@@ -13,6 +14,7 @@ import javax.inject.Named;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import lombok.Data;
+import org.primefaces.context.RequestContext;
 
 /**
  *
@@ -21,35 +23,34 @@ import lombok.Data;
 @Named("common")
 @RequestScoped
 @Data
-public class Common implements Serializable{
-    
-    private String username;
-    
-    private String password;
-    
-    
-   public String login() throws ServletException, NoSuchAlgorithmException{
-       
-       HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance()
-                                                                     .getExternalContext()
-                                                                     .getRequest();
-       
-   
-       
-       request.login(username, password);
+public class Common implements Serializable {
 
-       
-       return "index.xhtml?faces-redirect=true";
-       
-   }
-    
-    
-   public String logout(){
-       
-       FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-       
-       return "/login.xhtml?faces-redirect=true";
-       
-   }
-    
+    private String username;
+
+    private String password;
+
+    public String login() throws ServletException, NoSuchAlgorithmException, IOException {
+
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance()
+                .getExternalContext()
+                .getRequest();
+
+        try {
+            request.login(username, password);
+        }catch(Exception e){
+            RequestContext.getCurrentInstance().execute("setFailedLogin()");
+        }
+
+        return "index.xhtml?faces-redirect=true";
+
+    }
+
+    public String logout() {
+
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+
+        return "/login.xhtml?faces-redirect=true";
+
+    }
+
 }
